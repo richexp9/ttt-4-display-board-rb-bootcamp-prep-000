c3e202a72ce0aad3f9289dc2d80a7cb94ce6f8f2
Richard Loera <richexp9@gmx.com>
2017-12-30 13:18:18 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,5 +5,5 @@ def display_board(board)
   print "-----------\n"
   print " #{board[3]} | #{board[4]} | #{board[5]} \n"
   print "-----------\n"
-  print " #{board[0]} | #{board[0]} | #{board[6]} \n"
+  print " #{board[6]} | #{board[0]} | #{board[6]} \n"
 end
